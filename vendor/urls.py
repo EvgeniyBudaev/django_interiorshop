3016a63a375f169135d1eba7cee58dbad32e6a7d
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-03 15:17:08 +0300

    create simple area for vendors (for testing)

--- vendor/urls.py
+++ vendor/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-  path('become_vendor/', views.become_vendor, name='become_vendor')
+  path('become_vendor/', views.become_vendor, name='become_vendor'),
+  path('vendor_admin/', views.vendor_admin, name='vendor_admin')
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
@@ -19,3 +20,10 @@ def become_vendor(request):
     form = UserCreationForm()
 
   return render(request, 'vendor/become_vendor.html', {'form': form})
+
+
+@login_required
+def vendor_admin(request):
+  vendor = request.user.vendor
+
+  return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
