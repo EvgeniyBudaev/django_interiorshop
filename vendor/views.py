2750b3d8df822d9fc82a0163ca2e08fdd79e9d64
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-03 17:59:33 +0300

    Make it possible for vendors to add products

--- vendor/views.py
+++ vendor/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
 from django.shortcuts import render, redirect
 
 from .models import Vendor
 from product.models import Product
+
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -29,3 +32,20 @@ def vendor_admin(request):
   products = vendor.products.all()
 
   return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
+
+
+@login_required
+def add_product(request):
+  if request.method == 'POST':
+    form = ProductForm(request.POST, request.FILES)
+
+    if form.is_valid():
+      product = form.save(commit=False)
+      product.vendor = request.user.vendor
+      product.slug = slugify(product.title)
+      product.save()
+      return redirect('vendor_admin')
+  else:
+    form = ProductForm()
+
+  return render(request, 'vendor/add_product.html', {'form': form})
