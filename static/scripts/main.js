f0f294957d58f700e1c9dc99a8b8e9383cf5a326
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-03 14:53:56 +0300

    add app for vendors

---
+++ static/scripts/main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+    // тоже самое
+    // const $navbar = Array.from(document.querySelectorAll('.navbar-burger'))
+    // console.log('$navbarBurgers', $navbarBurgers)
+    console.log('$navbar', $navbar)
+    if ($navbarBurgers.length >0) {
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target
+                const $target = document.getElementById(target)
+
+                el.classList.toggle('is-active')
+                $target.classList.toggle('is-active')
+            })
+        })
+    }
+})
