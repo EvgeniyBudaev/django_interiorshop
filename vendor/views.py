8f49b2e0ce455ec4cbd46a8da14081294cc01106
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-04-03 15:10:01 +0300

    create view, template and form for sign up

--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .models import Vendor
 
 # Create your views here.
+
+
+def become_vendor(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      vendor = Vendor.objects.create(name=user.username, created_by=user)
+      return redirect('frontpage')
+  else:
+    form = UserCreationForm()
+
+  return render(request, 'vendor/become_vendor.html', {'form': form})
